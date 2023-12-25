0ee5c8a7af2793aeb640261583ea89db3b425967
Eduardo San Martin Morote <posva13@gmail.com>
2023-12-25 18:44:03 +0100

    build!: remove iife version
    Use import maps instead

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   // splitting: false,
   sourcemap: true,
   clean: true,
-  format: ['cjs', 'esm', 'iife'],
+  format: ['cjs', 'esm'],
   globalName: 'PiniaColada',
   external: ['vue', 'pinia'],
   dts: true,
