04c970f15528898b359318543746f08ca336f4b9
Eduardo San Martin Morote <posva13@gmail.com>
2024-01-07 22:18:13 +0100

    chore: add stringify fn

---
+++ src/utils.spec.ts
@@ -0,0 +1,29 @@
+import { describe, it, expect } from 'vitest'
+import { stringifyFlatObject } from './utils'
+
+describe('utils', () => {
+  describe('stringifyFlatObject', () => {
+    it('stringifies an object no matter the order of keys', () => {
+      expect(stringifyFlatObject({ a: 1, b: 2 })).toBe('{"a":1,"b":2}')
+      expect(stringifyFlatObject({ b: 2, a: 1 })).toBe('{"a":1,"b":2}')
+    })
+
+    it('works with all primitives', () => {
+      expect(stringifyFlatObject({ a: 1, b: 2 })).toBe('{"a":1,"b":2}')
+      expect(stringifyFlatObject({ a: '1', b: '2' })).toBe('{"a":"1","b":"2"}')
+      expect(stringifyFlatObject({ a: true, b: false })).toBe(
+        '{"a":true,"b":false}'
+      )
+      expect(stringifyFlatObject({ a: null })).toBe('{"a":null}')
+    })
+
+    it('works with arrays', () => {
+      expect(stringifyFlatObject({ a: [1, 2], b: [3, 4] })).toBe(
+        '{"a":[1,2],"b":[3,4]}'
+      )
+      expect(stringifyFlatObject({ b: [1, 2], a: [3, 4] })).toBe(
+        '{"a":[3,4],"b":[1,2]}'
+      )
+    })
+  })
+})
