4760612170ccc99213abcb45ad1090a7cba6c588
Eduardo San Martin Morote <posva13@gmail.com>
2024-02-19 15:18:12 +0100

    test: remove warns

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { effectScope, ref } from 'vue'
 import { describe, it, expect, afterEach, beforeEach, vi } from 'vitest'
 import { stringifyFlatObject, delayLoadingRef } from './utils'
 
@@ -36,53 +36,76 @@ describe('utils', () => {
       vi.restoreAllMocks()
     })
 
-    it('contains an initial value of false', () => {
-      const isLoading = ref(false)
-      const delayedRef = delayLoadingRef(isLoading, 100)
-      expect(delayedRef.value).toBe(false)
-    })
+    function witchinEffect(fn: () => void) {
+      return async () => {
+        const effect = effectScope(true)
+        await effect.run(fn)
+        effect.stop()
+      }
+    }
 
-    it('contains an initial value of true', () => {
-      const isLoading = ref(true)
-      const delayedRef = delayLoadingRef(isLoading, 100)
-      expect(delayedRef.value).toBe(true)
-    })
+    it(
+      'contains an initial value of false',
+      witchinEffect(() => {
+        const isLoading = ref(false)
+        const delayedRef = delayLoadingRef(isLoading, 100)
+        expect(delayedRef.value).toBe(false)
+      })
+    )
 
-    it('sets the value to true after a delay', async () => {
-      const isLoading = ref(false)
-      const delayedRef = delayLoadingRef(isLoading, 100)
-      expect(delayedRef.value).toBe(false)
+    it(
+      'contains an initial value of true',
+      witchinEffect(() => {
+        const isLoading = ref(true)
+        const delayedRef = delayLoadingRef(isLoading, 100)
+        expect(delayedRef.value).toBe(true)
+      })
+    )
 
-      isLoading.value = true
-      expect(delayedRef.value).toBe(false)
-      await vi.advanceTimersByTime(80)
-      expect(delayedRef.value).toBe(false)
+    it(
+      'sets the value to true after a delay',
+      witchinEffect(async () => {
+        const isLoading = ref(false)
+        const delayedRef = delayLoadingRef(isLoading, 100)
+        expect(delayedRef.value).toBe(false)
 
-      await vi.advanceTimersByTime(101)
-      expect(delayedRef.value).toBe(true)
-    })
+        isLoading.value = true
+        expect(delayedRef.value).toBe(false)
+        await vi.advanceTimersByTime(80)
+        expect(delayedRef.value).toBe(false)
 
-    it('sets the value to false immediately', async () => {
-      const isLoading = ref(true)
-      const delayedRef = delayLoadingRef(isLoading, 100)
+        await vi.advanceTimersByTime(101)
+        expect(delayedRef.value).toBe(true)
+      })
+    )
 
-      isLoading.value = false
-      expect(delayedRef.value).toBe(false)
-      await vi.advanceTimersByTime(101)
-      expect(delayedRef.value).toBe(false)
-    })
+    it(
+      'sets the value to false immediately',
+      witchinEffect(async () => {
+        const isLoading = ref(true)
+        const delayedRef = delayLoadingRef(isLoading, 100)
 
-    it('stops the timeout when the ref is set to false', async () => {
-      const isLoading = ref(false)
-      const delayedRef = delayLoadingRef(isLoading, 100)
+        isLoading.value = false
+        expect(delayedRef.value).toBe(false)
+        await vi.advanceTimersByTime(101)
+        expect(delayedRef.value).toBe(false)
+      })
+    )
 
-      isLoading.value = true
-      await vi.advanceTimersByTime(80)
-      isLoading.value = false
-      expect(delayedRef.value).toBe(false)
-      // even after waiting the delay
-      await vi.advanceTimersByTime(101)
-      expect(delayedRef.value).toBe(false)
-    })
+    it(
+      'stops the timeout when the ref is set to false',
+      witchinEffect(async () => {
+        const isLoading = ref(false)
+        const delayedRef = delayLoadingRef(isLoading, 100)
+
+        isLoading.value = true
+        await vi.advanceTimersByTime(80)
+        isLoading.value = false
+        expect(delayedRef.value).toBe(false)
+        // even after waiting the delay
+        await vi.advanceTimersByTime(101)
+        expect(delayedRef.value).toBe(false)
+      })
+    )
   })
 })
