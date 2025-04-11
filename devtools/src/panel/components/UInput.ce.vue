92e333da661c4b5e4542521d44a1cb2c6eb58fc4
Eduardo San Martin Morote <posva13@gmail.com>
2025-04-11 09:23:02 +0200

    chore: component

---
+++ devtools/src/panel/components/UInput.ce.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  defaultValue?: string | number
+}>()
+
+const modelValue = defineModel<string | number>()
+</script>
+
+<template>
+  <input v-model="modelValue" class="" >
+</template>
