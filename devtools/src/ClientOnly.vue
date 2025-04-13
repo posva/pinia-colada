6f37405f6101455069e6276ba7e226e2bf20d503
Eduardo San Martin Morote <posva13@gmail.com>
2025-04-13 23:30:29 +0200

    chore: client only

---
+++ devtools/src/ClientOnly.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const isMounted = ref(false)
+onMounted(() => {
+  isMounted.value = true
+})
+</script>
+
+<template>
+  <template v-if="isMounted">
+    <slot />
+  </template>
+</template>
