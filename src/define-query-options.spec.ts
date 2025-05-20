7f7387d1210e0deeafa9acbe769824e7e4a20c67
Eduardo San Martin Morote <posva13@gmail.com>
2025-05-20 11:41:17 +0200

    test: defineQuery with dynamyc options can be refetched

--- src/define-query-options.spec.ts
+++ src/define-query-options.spec.ts
@@ -5,6 +5,7 @@ import { ref } from 'vue'
 import { createPinia } from 'pinia'
 import { PiniaColada } from './pinia-colada'
 import { useQuery } from './use-query'
+import { useQueryCache } from './query-store'
 
 describe('defineQueryOptions', () => {
   it('can describe static options', () => {
@@ -70,6 +71,55 @@ describe('defineQueryOptions', () => {
       expect(query).toHaveBeenCalledWith(1)
       expect(query).toHaveBeenCalledTimes(2)
       expect(wrapper.text()).toBe('data:1')
+    })
+
+    // https://github.com/posva/pinia-colada/issues/287
+    it('can invalidate and refetch multiple queries at once', async () => {
+      let tag = 0 // allows to differentiate between calls
+      const query = vi.fn((id: number) => `data:${id} (${tag})`)
+
+      const opts = defineQueryOptions((id: number) => ({
+        key: ['a', id],
+        query: async () => query(id),
+      }))
+
+      const id = ref(0)
+      const pinia = createPinia()
+      const wrapper = mount(
+        {
+          setup() {
+            return {
+              ...useQuery(opts, id),
+            }
+          },
+          template: `<div>{{ data }}</div>`,
+        },
+        {
+          global: {
+            plugins: [pinia, PiniaColada],
+          },
+        },
+      )
+
+      // put in the cache multiple entries
+      await flushPromises()
+      id.value++
+      await flushPromises()
+      query.mockClear()
+
+      const queryCache = useQueryCache(pinia)
+      tag++ // change the data
+
+      queryCache.invalidateQueries({ key: ['a'] }, 'all')
+      expect(query).toHaveBeenCalledTimes(2)
+      expect(query).toHaveBeenCalledWith(0)
+      expect(query).toHaveBeenCalledWith(1)
+
+      await flushPromises()
+
+      expect(wrapper.text()).toBe('data:1 (1)')
+      // it was refetched correctly
+      expect(queryCache.getQueryData(['a', 0])).toBe('data:0 (1)')
     })
   })
 })
