9438cbd483361ecbc3c945e2a849c233fec6bad5
Eduardo San Martin Morote <posva13@gmail.com>
2025-05-20 11:30:23 +0200

    chore: format

--- src/entry-filter.ts
+++ src/entry-filter.ts
@@ -6,7 +6,6 @@ import type { EntryKey } from './entry-keys'
  *
  * @internal
  */
-
 export interface EntryFilter_Base<TEntry> {
   /**
    * A key to filter the entries.
@@ -48,27 +47,30 @@ export interface EntryFilter_Base<TEntry> {
    * @param entry - entry to filter
    */
   predicate?: (entry: TEntry) => boolean
-} /**
-   * Filter to get exactly one entry from the cache. Requires the `key` to be set.
-   *
-   * @internal
-   */
+}
 
+/**
+ * Filter to get exactly one entry from the cache. Requires the `key` to be set.
+ *
+ * @internal
+ */
 export interface EntryFilter_Key<TEntry> extends EntryFilter_Base<TEntry> {
   key: EntryKey
   exact: true
-} /**
-   * Filter to get multiple matching entries from the cache.
-   *
-   * @internal
-   */
+}
 
+/**
+ * Filter to get multiple matching entries from the cache.
+ *
+ * @internal
+ */
 export interface EntryFilter_NoKey<TEntry> extends EntryFilter_Base<TEntry> {
   exact?: false
-} /**
-   * Base interface to filter entries from a cache.
-   *
-   * @internal
-   */
+}
 
+/**
+ * Base interface to filter entries from a cache.
+ *
+ * @internal
+ */
 export type EntryFilter<TEntry> = EntryFilter_NoKey<TEntry> | EntryFilter_Key<TEntry>
