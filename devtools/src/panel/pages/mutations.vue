4d7ad5f6d206c1bfdcae368e5c50dd07aca83829
Eduardo San Martin Morote <posva13@gmail.com>
2025-05-21 15:10:10 +0200

    feat: wip page mutations

--- devtools/src/panel/pages/mutations.vue
+++ devtools/src/panel/pages/mutations.vue
@@ -1,3 +1,7 @@
 <template>
-  <p>TODO: mutations</p>
+  <section class="p-2">
+    <h2 class="font-extrabold text-2xl">Mutations</h2>
+
+    <p class="my-2 font-mono">🚧 Work in progress 🏗️</p>
+  </section>
 </template>
