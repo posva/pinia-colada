5125bb6e3248ba0d035031c46e22b0a4f88138e8
Eduardo San Martin Morote <posva13@gmail.com>
2025-05-23 23:59:06 +0200

    chore: test version iframe

--- devtools/src/panel/index.ts
+++ devtools/src/panel/index.ts
@@ -2,6 +2,8 @@ import { defineCustomElement } from 'vue'
 import DevtoolsPanelCE from './DevtoolsPanel.ce.vue'
 import { configureApp } from './configure-app'
 
+export { configureApp, defineCustomElement, DevtoolsPanelCE }
+
 export const DevtoolsPanel = defineCustomElement(DevtoolsPanelCE, {
   configureApp,
   shadowRoot: true,
