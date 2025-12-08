9e8ca93b6f69d65637af1aa20236f4d030c00e6f
Eduardo San Martin Morote <posva13@gmail.com>
2025-12-08 15:22:19 +0100

    style: format nuxt

--- nuxt/src/runtime/plugin.ts
+++ nuxt/src/runtime/plugin.ts
@@ -30,8 +30,7 @@ export default defineNuxtPlugin({
           queryCache.caches.clear()
         }
       })
-    }
-    else if (nuxtApp.payload && nuxtApp.payload.pinia_colada) {
+    } else if (nuxtApp.payload && nuxtApp.payload.pinia_colada) {
       // we are inside of an injectable context so `useQueryCache()` works
       hydrateQueryCache(queryCache, nuxtApp.payload.pinia_colada)
     }
