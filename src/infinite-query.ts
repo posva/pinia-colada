34e565550fd6ec896af14c63e30934c9b629e374
YusukeSano <yuu131129@gmail.com>
2026-02-26 02:14:41 +0900

    fix(infinite-query): allow null initialPageParam to continue pagination

--- src/infinite-query.ts
+++ src/infinite-query.ts
@@ -333,29 +333,23 @@ export function useInfiniteQuery<
       | NonNullable<TDataInitial>
       | undefined,
   ) {
-    const lastPageParam = data?.pageParams.at(-1)
-    nextPageParam.value =
-      lastPageParam != null
-        ? options.getNextPageParam(
-            // data is present if lastPageParam is not null
-            data!.pages.at(-1)!,
-            data!.pages,
-            lastPageParam,
-            data!.pageParams,
-          )
-        : null
+    nextPageParam.value = data?.pages.length
+      ? options.getNextPageParam(
+          data.pages.at(-1)!,
+          data.pages,
+          data.pageParams.at(-1)!,
+          data.pageParams,
+        )
+      : null
 
-    const firstPageParam = data?.pageParams.at(0)
-    previousPageParam.value =
-      firstPageParam != null
-        ? options.getPreviousPageParam?.(
-            // same as above
-            data!.pages.at(0)!,
-            data!.pages,
-            firstPageParam,
-            data!.pageParams,
-          )
-        : null
+    previousPageParam.value = data?.pages.length
+      ? options.getPreviousPageParam?.(
+          data.pages.at(0)!,
+          data.pages,
+          data.pageParams.at(0)!,
+          data.pageParams,
+        )
+      : null
   }
 
   // if we have initial data, we need to set the next and previous page params
