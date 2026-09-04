072fc2b9a848ef5d191086ff4fd39216923443f5
Eduardo San Martin Morote <posva13@gmail.com>
2026-09-04 15:17:49 +0200

    chore: router

--- devtools/src/app/router.ts
+++ devtools/src/app/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-export default createRouter({
+export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/queries' },
