f9dfe8d2c0b7d48410e52772e4d353fdfeacd7c1
Eduardo San Martin Morote <posva13@gmail.com>
2026-09-04 15:17:49 +0200

    chore: router

--- devtools/src/app/main.ts
+++ devtools/src/app/main.ts
@@ -2,7 +2,7 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import { PiniaColada } from '@pinia/colada'
 import App from './App.vue'
-import router from './router.ts'
+import { router } from './router.ts'
 import './style.css'
 
 createApp(App).use(createPinia()).use(PiniaColada).use(router).mount('#app')
