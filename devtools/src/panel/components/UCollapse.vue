3939904ae09ccff3e657240074180699c30ee045
Eduardo San Martin Morote <posva13@gmail.com>
2026-09-08 13:17:13 +0200

    feat: sticky titles

--- devtools/src/panel/components/UCollapse.vue
+++ devtools/src/panel/components/UCollapse.vue
@@ -64,16 +64,18 @@ function scrollAfterOpening(event: TransitionEvent) {
 
 <template>
   <div ref="collapse" class="collapse collapse-arrow" @transitionend.self="scrollAfterOpening">
-    <input v-model="open" type="checkbox" @change="prepareScroll" />
-    <div class="collapse-title px-2 py-0.5 bg-neutral-200 dark:bg-neutral-800 theme-neutral">
-      <slot name="title" :open :title>
-        <h3 class="font-semibold text-sm flex gap-x-1 items-center">
-          <slot name="icon">
-            <component :is="icon" v-if="icon" class="size-4" />
-          </slot>
-          {{ title ?? 'Group' }}
-        </h3>
-      </slot>
+    <div class="collapse-header">
+      <input v-model="open" type="checkbox" @change="prepareScroll" />
+      <div class="collapse-title px-2 py-0.5 bg-neutral-200 dark:bg-neutral-800 theme-neutral">
+        <slot name="title" :open :title>
+          <h3 class="font-semibold text-sm flex gap-x-1 items-center">
+            <slot name="icon">
+              <component :is="icon" v-if="icon" class="size-4" />
+            </slot>
+            {{ title ?? 'Group' }}
+          </h3>
+        </slot>
+      </div>
     </div>
     <div :class="!noPadding && 'px-2'" class="collapse-content text-sm overflow-hidden">
       <slot :open />
@@ -89,32 +91,36 @@ function scrollAfterOpening(event: TransitionEvent) {
 .collapse {
   position: relative;
   display: grid;
-  overflow: hidden;
+  overflow: clip;
   width: 100%;
   grid-template-rows: max-content 0fr;
   transition: grid-template-rows 0.2s;
   isolation: isolate;
 
-  > input:is([type='checkbox'], [type='radio']) {
-    grid-column-start: 1;
-    grid-row-start: 1;
+  > .collapse-header > input:is([type='checkbox'], [type='radio']) {
+    position: absolute;
+    inset: 0;
     appearance: none;
     opacity: 0;
+    cursor: inherit;
   }
 
   &:is([open], :focus),
-  &:has(> input:is([type='checkbox'], [type='radio']):checked) {
+  &:has(> .collapse-header > input:is([type='checkbox'], [type='radio']):checked) {
     grid-template-rows: max-content 1fr;
   }
 
   &:is([open], :focus) > .collapse-content,
   &:not(.collapse-close)
-    > :where(input:is([type='checkbox'], [type='radio']):checked ~ .collapse-content) {
+    > :where(
+      .collapse-header:has(> input:is([type='checkbox'], [type='radio']):checked)
+        ~ .collapse-content
+    ) {
     visibility: visible;
     min-height: fit-content;
   }
   &:focus-visible,
-  &:has(> input:is([type='checkbox'], [type='radio']):focus-visible) {
+  &:has(> .collapse-header > input:is([type='checkbox'], [type='radio']):focus-visible) {
     outline-color: var(--ui-text);
     outline-style: solid;
     outline-width: 2px;
@@ -122,44 +128,51 @@ function scrollAfterOpening(event: TransitionEvent) {
   }
 
   &:not(.collapse-close) {
-    > input[type='checkbox'],
-    > input[type='radio']:not(:checked),
-    > .collapse-title {
+    > .collapse-header {
       cursor: pointer;
     }
   }
 
-  &:focus:not(.collapse-close, .collapse[open]) > .collapse-title {
+  &:focus:not(.collapse-close, .collapse[open]) > .collapse-header {
     cursor: unset;
   }
 
   &:is([open]) {
     &.collapse-arrow {
-      > .collapse-title:after {
+      > .collapse-header > .collapse-title:after {
         transform: translateY(-50%) rotate(225deg);
       }
     }
   }
 
   &.collapse-arrow:focus {
-    > .collapse-title:after {
+    > .collapse-header > .collapse-title:after {
       transform: translateY(-50%) rotate(225deg);
     }
   }
 
   &.collapse-arrow {
-    > input:is([type='checkbox'], [type='radio']):checked ~ .collapse-title:after {
+    > .collapse-header
+      > input:is([type='checkbox'], [type='radio']):checked
+      ~ .collapse-title:after {
       transform: translateY(-50%) rotate(225deg);
     }
   }
 
-  > input:is([type='checkbox'], [type='radio']) {
+  > .collapse-header > input:is([type='checkbox'], [type='radio']) {
     z-index: 1;
     width: 100%;
   }
+
+  &:has(> .collapse-header > input:is([type='checkbox'], [type='radio']):checked)
+    > .collapse-header {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+  }
 }
 
-.collapse-title,
+.collapse-header,
 .collapse-content {
   grid-column-start: 1;
   grid-row-start: 1;
@@ -175,7 +188,7 @@ function scrollAfterOpening(event: TransitionEvent) {
 }
 
 .collapse-arrow {
-  > .collapse-title:after {
+  > .collapse-header > .collapse-title:after {
     position: absolute;
     display: block;
     height: 0.5rem;
@@ -192,6 +205,10 @@ function scrollAfterOpening(event: TransitionEvent) {
     box-shadow: 2px 2px;
     pointer-events: none;
   }
+}
+
+.collapse-header {
+  position: relative;
 }
 
 .collapse-title {
